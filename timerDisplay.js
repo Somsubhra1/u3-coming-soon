7070a5c69efba960d4301b80d70911d48a693f09
Somsubhra Das <das.somsubhra1@gmail.com>
2019-11-20 11:58:09 +0530

    Timer created and background opacity fixed

--- timerDisplay.js
+++ timerDisplay.js
@@ -1,8 +1,32 @@
-let revealBtn = document.querySelector('.reveal_btn'),
-left = document.querySelector('.left'),
-right = document.querySelector('.right');
+let revealBtn = document.querySelector(".reveal_btn"),
+    left = document.querySelector(".left"),
+    right = document.querySelector(".right");
 
-revealBtn.addEventListener('click', () => {
+revealBtn.addEventListener("click", () => {
     left.style.left = "-50%";
     right.style.left = "100%";
 });
+
+const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+let countDown = new Date("Dec 1, 2019 00:00:00").getTime(),
+    x = setInterval(function() {
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        (document.getElementById("days").innerText = Math.floor(
+            distance / day
+        )),
+            (document.getElementById("hours").innerText = Math.floor(
+                (distance % day) / hour
+            )),
+            (document.getElementById("minutes").innerText = Math.floor(
+                (distance % hour) / minute
+            )),
+            (document.getElementById("seconds").innerText = Math.floor(
+                (distance % minute) / second
+            ));
+    }, second);
